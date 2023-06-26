e7c0a1774de91284b588159c133ecdff83955b89
Dhananjay Tyagi <tyagi@Dhananjays-MacBook-Pro.local>
2023-06-25 19:51:38 -0700

    added task model
    '

---
+++ Enums/ImportanceEnum.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Tasks.API1.Enums
+{
+	public enum ImportanceEnum
+	{
+		High,
+		Medium,
+		Low
+	}
+}
+
---
+++ Models/Task.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Tasks.API1.Enums;
+
+namespace Tasks.API1.Models
+{
+	public class Task
+	{
+		[Required]
+		public string Description { get; set; } = string.Empty;
+
+        [Required]
+        public ImportanceEnum Importance { get; set; }
+
+        [Required]
+        public DateTime TimeTaken { get; set; }
+
+        public bool IsCompleted { get; set; }
+
+		public DateTime CreatedAt { get; set; }
+
+		public DateTime TargetDate { get; set; }
+	}
+}
+
